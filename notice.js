f0eb3594f8777011fcc48be959f4eb96ad661e6d
Ashiish.me <iloveh4ck1ng@gmail.com>
2020-07-26 22:03:41 +0545

    chore: initialize project & added pre-install notice

---
+++ notice.js
@@ -0,0 +1,7 @@
+
+/**
+ * Add notice before installation
+ */
+console.log('\x1b[33m%s\x1b[0m', 'WARNING:', 'Under development package.');
+console.log('\x1b[30m\x1b[43m%s\x1b[0m', 'NOTICE: Initial development is in progress, but there has not yet been a stable, usable release suitable for the public.');
+console.log(" ");
